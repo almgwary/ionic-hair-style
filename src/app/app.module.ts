949ddf89bd79bdc4e98bb9a58c14285ee618eb79
almgwary <almgwaryfci@gmail.com>
2017-09-14 15:22:26 +0200

    install  and add angular2 image gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,30 @@ import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { Angular2ImageGalleryModule  } from 'angular2-image-gallery'
+import 'web-animations-js/web-animations.min';
+import 'hammerjs/hammer';
+
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/function';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/number';
+import 'core-js/es6/math';
+import 'core-js/es6/string';
+import 'core-js/es6/date';
+import 'core-js/es6/array';
+import 'core-js/es6/regexp';
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+
+import 'web-animations-js';
+
 
 @NgModule({
   declarations: [
@@ -18,6 +41,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    BrowserAnimationsModule,
+    Angular2ImageGalleryModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [

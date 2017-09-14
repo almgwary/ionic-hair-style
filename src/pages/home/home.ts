dcd3733cef553e844e0de0ca4f6d9aad24bf390a
almgwary <almgwaryfci@gmail.com>
2017-09-14 15:22:48 +0200

    use image gallary

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,7 +7,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  flexImageSize:any=7;
+  flexBorderSize:any=3;
+  galleryName:any="yourGalleryName";
+  galleryDataUri:any="assets/data.json";
   constructor(public navCtrl: NavController) {
+
+  }
+
+  onGalleryViewerChange(e?){
 
   }
 
